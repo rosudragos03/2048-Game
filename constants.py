ce26b38c4e5c5f92d0917f90aa7cbefe14c353bd
rosudragos03 <03rosu@gmail.com>
2024-06-06 00:48:22 +0300

    game

---
+++ constants.py
@@ -0,0 +1,65 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#92877f"
+BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
+
+BACKGROUND_COLOR_DICT = {
+    2:      "#ffe4e1",  # Misty Rose
+    4:      "#ffefd5",  # Papaya Whip
+    8:      "#ffdab9",  # Peach Puff
+    16:     "#e6e6fa",  # Lavender
+    32:     "#dda0dd",  # Plum
+    64:     "#ee82ee",  # Violet
+    128:    "#d8bfd8",  # Thistle
+    256:    "#dda0dd",  # Plum
+    512:    "#da70d6",  # Orchid
+    1024:   "#ba55d3",  # Medium Orchid
+    2048:   "#9932cc",  # Dark Orchid
+    4096:   "#9400d3",  # Dark Violet
+    8192:   "#8a2be2",  # Blue Violet
+    16384:  "#8b008b",  # Dark Magenta
+    32768:  "#800080",  # Purple
+    65536:  "#4b0082",  # Indigo
+}
+
+CELL_COLOR_DICT = {
+    2:      "#000000",  # Black
+    4:      "#000000",  # Black
+    8:      "#ffffff",  # White
+    16:     "#ffffff",  # White
+    32:     "#ffffff",  # White
+    64:     "#ffffff",  # White
+    128:    "#ffffff",  # White
+    256:    "#ffffff",  # White
+    512:    "#ffffff",  # White
+    1024:   "#ffffff",  # White
+    2048:   "#ffffff",  # White
+    4096:   "#000000",  # Black
+    8192:   "#ffffff",  # White
+    16384:  "#000000",  # Black
+    32768:  "#000000",  # Black
+    65536:  "#ffffff",  # White
+}
+
+
+FONT = ("Verdana",40,"bold")
+
+KEY_QUIT = "Escape"
+KEY_BACK = "b"
+
+KEY_UP = "Up"
+KEY_DOWN = "Down"
+KEY_LEFT = "Left"
+KEY_RIGHT = "Right"
+
+KEY_UP_ALT1 = "w"
+KEY_DOWN_ALT1 = "s"
+KEY_LEFT_ALT1 = "a"
+KEY_RIGHT_ALT1 = "d"
+
+KEY_UP_ALT2 = "i"
+KEY_DOWN_ALT2 = "k"
+KEY_LEFT_ALT2 = "j"
+KEY_RIGHT_ALT2 = "l"
